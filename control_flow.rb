76ab4c2def8a2d507baeeefda3f2d2f4eea4128e
Caitlin Ma <109817775+kissology@users.noreply.github.com>
2022-12-12 10:26:15 -0500

    commit tree da3377534ded5d882c58419d39988ed1f6d652e2

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,55 @@
 def admin_login(username, password)
-  # your code here
+password = '12345'
+case password
+when 'admin'
+  'Access granted'
+when 'ADMIN'
+  'Access granted'
+else
+  'Access denied'
+  end
 end
-
+  
 def hows_the_weather(temperature)
-  # your code here
+if temperature < 40
+  "It's brisk out there!"
+elsif temperature < 65 && temperature > 40
+  "It's a little chilly out there!"
+elsif temperature > 85
+  "It's too dang hot out there!"
+else 
+  "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+case num
+when num % 3
+  'Fizz'
+when num % 5
+  'Buzz'
+when num % 3 && num % 5
+  "FizzBuzz"
+else
+  return num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+
+  when num1 + num2
+    "+"
+  when num1 - num2
+    "-"
+  when num1 * num2
+    "*"
+  when num1 / num2
+    "/"
+  else
+    "Invalid operation"
+  end
 end
+
+
 
